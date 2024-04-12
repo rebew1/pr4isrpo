215d464f7bda57acc1011dbbe5b80e2db6df808a
qweem <qweem@DESKTOP-BPIN3E9>
2024-04-12 23:08:59 +0700

    Добавьте файлы проекта.

---
+++ pr 4 isrpo.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    double o, v, z, s, n, m;
+    z = 10;
+    s = 4;
+    o = z / s;
+    cout << o;
+}

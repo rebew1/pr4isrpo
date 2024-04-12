de03304bbe24fc94819882c771cc78450d58f438
qweem <qweem@DESKTOP-BPIN3E9>
2024-04-12 23:14:31 +0700

    Geniy

--- pr 4 isrpo.cpp
+++ pr 4 isrpo.cpp
@@ -5,11 +5,13 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "rus");
-    double o, v, z, s, n, m;
+    double o, v, z, s, n;
     z = 10;
     s = 4;
     o = z / s;
     cout << o << endl;
     v = z * s;
     cout << v << endl;
+    n = z * z - s;
+    cout << n << endl;
 }
